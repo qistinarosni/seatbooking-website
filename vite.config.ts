d42f1374b919b3f5b36bfd369b4c9ec90de2d589
qistinarosni <qmr140901@gmail.com>
2026-06-17 20:49:32 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
+  base: '/seatbooking-website/',
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
